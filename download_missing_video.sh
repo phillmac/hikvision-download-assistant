24e2ca45f1ab4e4d52e586781339d3ec885c6063
phillmac <phillmackintosh@gmail.com>
2022-08-11 13:57:27 +0000

    Fix script

--- download_missing_video.sh
+++ download_missing_video.sh
@@ -11,7 +11,8 @@ echo '#!/usr/bin/env bash' > ./download.sh
 echo 'set -eo pipefail' >> ./download.sh
 
 
-java -jar ../target/hikvision-download-assistant-1.0-SNAPSHOT-jar-with-dependencies.jar --quiet --output json  "${@}" | jq -r --compact-output '.results[]' > results.json
+java -jar ../target/hikvision-download-assistant-1.0-SNAPSHOT-jar-with-dependencies.jar --quiet --output json  "${@}" > output.json
+cat output.json | jq -r --compact-output '.results[]' > results.json
 
 while read -r fname curlcmd
 do
