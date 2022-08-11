fd5d879efcf813422fc542e5853a09fd627ccf30
phillmac <phillmackintosh@gmail.com>
2022-08-11 08:55:48 +0000

    Install jq, add download missing video

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 FROM eclipse-temurin:11-jre
+RUN apt update && apt install -y jq
+
+FROM eclipse-temurin:11-jre
+
+COPY --from=0 /usr/bin/jq /usr/bin/jq
 
 COPY target/hikvision-download-assistant-1.0-SNAPSHOT-jar-with-dependencies.jar /target/hikvision-download-assistant-1.0-SNAPSHOT-jar-with-dependencies.jar
 COPY download_video.sh /usr/local/bin/download_video
+COPY  download_missing_video.sh /usr/local/bin/download_missing_video
 
-ENTRYPOINT ["download_video"]
+
+ENTRYPOINT ["download_missing_video"]
---
+++ download_missing_video.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ ! -d output ]
+then
+    mkdir -v output
+fi
+
+cd output
+
+echo '#!/usr/bin/env bash' > ./download.sh
+echo 'set -eo pipefail' >> ./download.sh
+
+
+java -jar ../target/hikvision-download-assistant-1.0-SNAPSHOT-jar-with-dependencies.jar --quiet --output json  "${@}" | jq -r --compact-output '.results[]' > results.json
+
+while read -r fname curlcmd
+    if [[-f "${fname}" ]]
+    then
+        echo "${curlcmd}" >> ./download.sh
+    else
+        echo "Skipping already downloaded ${fname}"
+    fi
+done < <(jq -r . "\(.outputFilename) \(.curlCommand)"  < results.json)
+
+chmod u+x ./download.sh
+
+./download.sh
