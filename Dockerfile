14e3806a10618366abc5c80909e55ad0a22b26b7
phillmac <phillmackintosh@gmail.com>
2022-09-19 08:32:29 +0000

    Add download missing pics cmd

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,8 @@ COPY --from=0 /usr/lib/x86_64-linux-gnu/libjq.so.1 /usr/lib/x86_64-linux-gnu/lib
 
 COPY target/hikvision-download-assistant-1.0-SNAPSHOT-jar-with-dependencies.jar /target/hikvision-download-assistant-1.0-SNAPSHOT-jar-with-dependencies.jar
 COPY download_video.sh /usr/local/bin/download_video
-COPY  download_missing_video.sh /usr/local/bin/download_missing_video
+COPY download_missing_video.sh /usr/local/bin/download_missing_video
+COPY download_missing_pics.sh /usr/local/bin/download_missing_pics
 
 
 ENTRYPOINT ["download_missing_video"]
---
+++ download_missing_pics.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ ! -d output ]
+then
+    mkdir -v output
+fi
+
+cd output || exit
+
+echo '#!/usr/bin/env bash' > ./download.sh
+echo 'set -eo pipefail' >> ./download.sh
+
+
+java -jar ../target/hikvision-download-assistant-1.0-SNAPSHOT-jar-with-dependencies.jar --quiet --output json  "${@}" > output.json
+cat output.json | jq -r --compact-output '.results[]' > results.json
+
+while read -r mediatype fname curlcmd
+do
+    if [[ "${mediatype}" == "PICTURE" ]]
+    then
+        echo "Checking ${fname}"
+        if [[ -f "${fname}" ]]
+        then
+            echo "Skipping already downloaded ${fname}"
+        else
+            echo "${curlcmd}" >> ./download.sh
+        fi
+    fi
+done < <(jq -r  '. | "\(.mediaType) \(.outputFilename) \(.curlCommand)"'  < results.json)
+
+chmod u+x ./download.sh
+
+./download.sh
