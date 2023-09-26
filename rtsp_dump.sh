014e904a4528bcd2ddf1cbad7d1d1fff4cab6e86
phillmac <phillmackintosh@gmail.com>
2023-09-26 06:02:23 +0000

    Include dt stamp

--- rtsp_dump.sh
+++ rtsp_dump.sh
@@ -6,8 +6,8 @@ openRTSP \
     -B 10000000 \
     -b 10000000 \
     -Q \
-    -F "${OUTPUT_PREFIX}" \
-    -p "${REC_SEG_LEN}" \
+    -F "${OUTPUT_PREFIX}_${date '+%F %T'}_" \
+    -P "${REC_SEG_LEN}" \
     -t \
     -u "${RTSP_USER}" "${RTSP_PASSWD}" \
     "rtsp://${RTSP_HOST}:${RTSP_PORT}/Streaming/Channels/${RTSP_CHAN}"
