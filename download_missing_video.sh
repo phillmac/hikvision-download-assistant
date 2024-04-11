89e1d2d64ad611496d146992a64a96f053e946a5
Phillip Mackintosh <phillip.mackintosh@gpwgroup.com.au>
2024-04-12 08:58:14 +1000

    Short circuit on failure

--- download_missing_video.sh
+++ download_missing_video.sh
@@ -20,45 +20,49 @@ then
     failed=1
 fi
 
-cat output.json | jq -r --compact-output '.results[]' > results.json
-((pcount=1))
-((tcount=0))
-echo '' > ./download.sh.tmp
+if ! ((failed))
+then
+    cat output.json | jq -r --compact-output '.results[]' > results.json
+    ((pcount=1))
+    ((tcount=0))
+    echo '' > ./download.sh.tmp
 
-while read -r mediatype fname curlcmd
-do
-    if [[ "${mediatype}" == "VIDEO" ]]
-    then
-        ((tcount=tcount+1))
-        echo "Checking ${fname} [${tcount}]"
-        if [[ -f "${output_dir}/${fname}" ]]
+    while read -r mediatype fname curlcmd
+    do
+        if [[ "${mediatype}" == "VIDEO" ]]
         then
-            echo "Skipping already downloaded ${fname}"
-        else
-            echo 'echo "$(date) Fetching '"${fname}"' ['${pcount}'/${fcount}/${tcount}]" | tee progress.log.txt' >> ./download.sh.tmp
-            echo "${curlcmd}" '2> >(tee curl.log.txt >&2)' >> ./download.sh.tmp
-            echo "mv -v '${output_dir}/tmp/${fname}' '${output_dir}'" >> ./download.sh.tmp
-            ((pcount=pcount+1))
+            ((tcount=tcount+1))
+            echo "Checking ${fname} [${tcount}]"
+            if [[ -f "${output_dir}/${fname}" ]]
+            then
+                echo "Skipping already downloaded ${fname}"
+            else
+                echo 'echo "$(date) Fetching '"${fname}"' ['${pcount}'/${fcount}/${tcount}]" | tee progress.log.txt' >> ./download.sh.tmp
+                echo "${curlcmd}" '2> >(tee curl.log.txt >&2)' >> ./download.sh.tmp
+                echo "mv -v '${output_dir}/tmp/${fname}' '${output_dir}'" >> ./download.sh.tmp
+                ((pcount=pcount+1))
+            fi
         fi
+    done < <(jq -r  '. | "\(.mediaType) \(.outputFilename) \(.curlCommand)"'  < results.json)
+
+    if ((tcount < 1))
+    then
+        echo "$(date) ~ Failed to find any assets"
+        failed=1
     fi
-done < <(jq -r  '. | "\(.mediaType) \(.outputFilename) \(.curlCommand)"'  < results.json)
 
-if ((tcount < 1))
-then
-    echo "$(date) ~ Failed to find any assets"
-    failed=1
-fi
+    echo "tcount=${tcount}" >> ./download.sh
+    echo "fcount=$((pcount - 1 ))" >> ./download.sh
 
-echo "tcount=${tcount}" >> ./download.sh
-echo "fcount=$((pcount - 1 ))" >> ./download.sh
+    cat ./download.sh.tmp >> ./download.sh
 
-cat ./download.sh.tmp >> ./download.sh
+    chmod u+x ./download.sh
 
-chmod u+x ./download.sh
+    if ! ./download.sh
+    then
+        failed=1
+    fi
 
-if ! ./download.sh
-then
-    failed=1
 fi
 
 cd / && rm -rvf "${output_dir}/tmp"
